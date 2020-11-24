c88371c4278a08d0a20ff6d581f6d91d82753a45
Winrida <winrida@yandex.ru>
2020-11-24 13:08:23 +0300

    Initial commit

---
+++ src/ru/mirea/pr9/EmptyStringException.java
@@ -0,0 +1,8 @@
+package ru.mirea.pr9;
+
+public class EmptyStringException extends Exception {
+
+    public EmptyStringException(String message) {
+        super(message);
+    }
+}
---
+++ src/ru/mirea/pr9/StudentNotFoundException.java
@@ -0,0 +1,8 @@
+package ru.mirea.pr9;
+
+public class StudentNotFoundException extends Exception {
+
+    public StudentNotFoundException(String message) {
+        super(message);
+    }
+}
